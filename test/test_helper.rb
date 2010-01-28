f51f02049341a495d3285a26aefdcbe5820c04eb
Keith Morrison <keithm@infused.org>
2010-01-28 00:42:50 -0800

    Add quietbacktrace

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,9 +3,15 @@ require 'rubygems'
 require 'test/unit'
 require 'mocha'
 require 'shoulda'
+require 'quietbacktrace'
 require 'gedspec'
 
 class Test::Unit::TestCase
+  new_backtrace_silencer :shoulda do |line|
+    line.include? 'gem/ruby'
+  end
+  backtrace_silencers << :shoulda
+  
   def fixture_file(filename)
     File.join(File.dirname(__FILE__), 'fixtures', filename)
   end
