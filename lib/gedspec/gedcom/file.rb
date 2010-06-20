8a0196504f0f6dd111ce71f48f8a5568b79ee587
Keith Morrison <keithm@infused.org>
2010-06-20 11:06:23 -0700

    Support extracting multiple records

--- lib/gedspec/gedcom/file.rb
+++ lib/gedspec/gedcom/file.rb
@@ -9,41 +9,42 @@ module Gedspec
         extract 'HEAD'
       end
     
-      def subm(xref)
+      def subm(xref = nil)
         extract 'SUBM', xref
       end
     
-      def indi(xref)
+      def indi(xref = nil)
         extract 'INDI', xref
       end
     
-      def fam(xref)
+      def fam(xref = nil)
         extract 'FAM', xref
       end
     
-      def repo(xref)
+      def repo(xref = nil)
         extract 'REPO', xref
       end
     
-      def sour(xref)
+      def sour(xref = nil)
         extract 'SOUR', xref
       end
     
-      def note(xref)
+      def note(xref = nil)
         extract 'NOTE', xref
       end
     
-      def obje(xref)
+      def obje(xref = nil)
         extract 'OBJE', xref
       end
       
       def record_regex(tag, xref, level)
-        xref ? /(^#{level} @#{xref}@ #{tag}.+?)^#{level}/m : /(^0 #{tag}.+?)^0/m
+        xref ? /(^#{level} @#{xref}@ #{tag}.+?)^#{level}/m : /(^0 (?:@[^@]+@ |)#{tag}.+?)(?=^0)/m
       end
     
       def extract(tag, xref = nil, level = 0)
+        @file.rewind
         results = @file.read.scan(record_regex(tag, xref, level))
-        results.size == 1 ? results.flatten[0] : results
+        results.size == 1 ? results.flatten[0] : results.flatten
       end
     
     end
--- test/unit/gedcom/file_test.rb
+++ test/unit/gedcom/file_test.rb
@@ -14,32 +14,67 @@ class Gedspec::Gedcom::FileTest < Test::Unit::TestCase
     assert_equal fixture_content('tcg551_head.ged'), @gedcom.head
   end
   
-  should 'extract submitter snippet' do
+  should 'extract a specific subm snippet' do
     assert_equal fixture_content('tcg551_sm2.ged'), @gedcom.subm('SM2')
   end
   
-  should 'extract indi snippet' do
+  should 'extract all subm snippets' do
+    assert_equal 3, @gedcom.subm.size
+    assert @gedcom.subm.include?(fixture_content('tcg551_sm2.ged'))
+  end
+  
+  should 'extract a specific indi snippet' do
     assert_equal fixture_content('tcg551_i13.ged'), @gedcom.indi('I13')
   end
   
-  should 'extract fam snippet' do
+  should 'extract all indi snippets' do
+    assert_equal 15, @gedcom.indi.size
+    assert @gedcom.indi.include?(fixture_content('tcg551_i13.ged'))
+  end
+  
+  should 'extract a specific fam snippet' do
     assert_equal fixture_content('tcg551_f5.ged'), @gedcom.fam('F5')
   end
   
-  should 'extract repo snippet' do
+  should 'extract all fam snippets' do
+    assert_equal 7, @gedcom.fam.size
+    assert @gedcom.fam.include?(fixture_content('tcg551_f5.ged'))
+  end
+  
+  should 'extract a specific repo snippet' do
     assert_equal fixture_content('tcg551_r1.ged'), @gedcom.repo('R1')
   end
   
-  should 'extract sour snippet' do
+  should 'extract all repo snippets' do
+    assert_equal 455, @gedcom.repo.size
+    assert @gedcom.repo.include?(fixture_content('tcg551_r1.ged'))
+  end
+  
+  should 'extract a specific sour snippet' do
     assert_equal fixture_content('tcg551_sr2.ged'), @gedcom.sour('SR2')
   end
   
-  should 'extract note snippet' do
+  should 'extract all sour snippets' do
+    assert_equal 2, @gedcom.sour.size
+    assert @gedcom.sour.include?(fixture_content('tcg551_sr2.ged'))
+  end
+  
+  should 'extract a specific note snippet' do
     assert_equal fixture_content('tcg551_n2.ged'), @gedcom.note('N2')
   end
   
-  should 'extact obje snippet' do
+  should 'extract all note snippets' do
+    assert_equal 33, @gedcom.note.size
+    assert @gedcom.note.include?(fixture_content('tcg551_n2.ged'))
+  end
+  
+  should 'extract a specific obje snippet' do
     assert_equal fixture_content('tcg551_m1.ged'), @gedcom.obje('M1')
+  end
+  
+  should 'extract all obje snippets' do
+    assert_equal 745, @gedcom.obje.size
+    assert @gedcom.obje.include?(fixture_content('tcg551_m1.ged'))
   end
   
 end
