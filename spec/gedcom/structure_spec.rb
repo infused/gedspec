6dfc24650f6e444b29d886707b06b25690e77022
Keith Morrison <keithm@infused.org>
2011-01-06 20:54:48 -0800

    A little better structure specs

--- spec/gedcom/structure_spec.rb
+++ spec/gedcom/structure_spec.rb
@@ -1,15 +1,25 @@
 require 'spec_helper'
 
 describe Gedspec::Gedcom::Structure do
+  before { @structure = Gedspec::Gedcom::Structure.new }
+  
   describe '#get_level' do
-    it 'grasb the first number from the string' do
-      @structure = Gedspec::Gedcom::Structure.new
-      @structure.get_level('0 @I1@ INDI').should == 0
+    describe 'when the input string contains a digit character (0123456789)' do
+      it 'returns the first digit character as an integer', :shared => true do
+        @structure.get_level('0 @I1@ INDI').should == 0
+      end
     end
     
-    it 'should be nil if the the string does not start with a number' do
-      @structure = Gedspec::Gedcom::Structure.new
-      @structure.get_level('INDI').should be_nil
+    describe 'when the input string contains white-space before the digit' do
+      it 'returns the first digit character as an integer' do
+        @structure.get_level(' 0 @I1@ INDI').should == 0
+      end
+    end
+    
+    describe 'when the input string does not contain a digit character' do
+      it 'should be nil if the the string does not contain a number' do
+        @structure.get_level('INDI').should be_nil
+      end
     end
   end
 end
